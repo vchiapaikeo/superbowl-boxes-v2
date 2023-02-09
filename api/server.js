ca01fc67b3063834a2cd3f648781c6e42dd9ab81
Victor Chiapaikeo <vchiapaikeo@gmail.com>
2023-02-09 08:47:28 -0500

    Add backend

---
+++ api/server.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+
+// TODO: Upgrade to Express 5 once it is out of alpha and remove this wrapper.
+// Express 4.x does not support automatic asynchronous error handling and each route needs
+// to handle rejected promises on their own. This middleware catches rejected promises for
+// every route in the overridden methods to format a 500 response and json error message.
+const asyncHandler = fn => (req, res, next) => {
+  // default next handler sends back a webpage
+  return Promise
+    .resolve(fn(req, res, next))
+    .catch( (error) => {
+      const errorCode = error.status || 500
+      console.error(`AsyncRouteHandlingError: ${error.name} - ${error.message}`)
+      console.error(error)
+      res.status(errorCode).send({ error: error.message })
+    })
+}
+
+const asyncOverrideMethods = [
+  'get',
+  'post',
+  'delete'
+]
+
+function toAsyncRouter(router) {
+  for (let key in router) {
+    if (asyncOverrideMethods.includes(key)) {
+      let method = router[key]
+      router[key] = (path, ...callbacks) => method.call(router, path, ...callbacks.map(cb => asyncHandler(cb)))
+    }
+  }
+  return router
+}
+
+const app = toAsyncRouter(express())
+const jsonParser = bodyParser.json()
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Helper functions
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+// API routes
+app.get("/api/v1/names", async (req, res) => {
+  const includeBlanks = req.query.includeBlanks;
+
+  const names = ["Victor", "Tue", "Haruka", "Jay", "Ephraim", "Linh", "Ryan", "Aya", "Mae"];
+  let nonshuffled = [];
+  const numArraysNeeded = parseInt(100 / names.length, 10);
+  for (let i = 0; i < numArraysNeeded; i++) {
+    nonshuffled = nonshuffled.concat(names);
+  }
+
+  const need = 100 - nonshuffled.length;
+  if (includeBlanks === true || includeBlanks === "true") {
+    for (let i = 0; i < need; i++) {
+        nonshuffled.push("-")
+    }
+  } else {
+    const shuffledNames = shuffle(names);
+    for (let i = 0; i < need; i++) {
+        nonshuffled.push(shuffledNames[i])
+    }
+  }
+
+  const shuffled = shuffle(nonshuffled);
+
+  res.send({names: shuffled});
+});
+
+app.get("/api/v1/readiness_check", async (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get("/api/v1/liveness_check", async (req, res) => {
+  res.sendStatus(200);
+});
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+module.exports = app
---
+++ client/src/App.js
@@ -0,0 +1,186 @@
+import React, { useState, useEffect } from "react";
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import HomeIcon from '@mui/icons-material/Home';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import LinearProgress from '@mui/material/LinearProgress';
+import Link from '@mui/material/Link';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import Tooltip from '@mui/material/Tooltip';
+import { styled } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        superbowl-boxes-v2
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+const Header = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.primary,
+}));
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const getHeader = (index, value) => {
+  return (
+    <Grid item xs={1} key={index}>
+      <Header><b>{value}</b></Header>
+    </Grid>
+  );
+}
+
+const getItem = (index, value) => {
+  return (
+    <Grid item xs={1} key={index}>
+      <Item>{value}</Item>
+    </Grid>
+  );
+}
+
+const getGridItem = (index, names) => {
+  if (index === 0) {
+    return getItem(index, "-");
+  } else if (index >= 1 && index <= 10) {
+    return getHeader(index, index - 1);
+  } else if (index > 10 && index % 11 === 0) {
+    return getHeader(index, index / 11 - 1);
+  }
+  const subtractor = parseInt(index / 11, 10) - 1;
+  const convertedIndex = index - 12 - subtractor;
+  return getItem(index, names[convertedIndex]);
+};
+
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [names, setNames] = useState([]);
+  const [save, setSave] = useState(false);
+  const [shouldIncludeBlanks, setShouldIncludeBlanks] = useState(false);
+
+  async function getShuffledNames(shouldIncludeBlanks) {
+    const response = await fetch(`/api/v1/names?includeBlanks=${shouldIncludeBlanks}`);
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  }
+
+  function loadNames() {
+    if (save === true) {
+      return;
+    }
+    getShuffledNames(shouldIncludeBlanks)
+      .then((res) => {
+        console.log(res);
+        setLoading(false);
+        setNames(res.names);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    loadNames();
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <HomeIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Hometown Fam Super Bowl Boxes 2023 Edition
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {/* Main */}
+      <main>
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 6,
+            pb: 4,
+          }}
+        >
+          <Stack
+            sx={{ pt: 1 }}
+            direction="row"
+            spacing={4}
+            justifyContent="center"
+          >
+            <Button variant="contained" onClick={() => loadNames()} disabled={save}>Randomize</Button>
+            <Button variant={save ? "outlined" : "contained"} onClick={() => setSave(true)}>Save</Button>
+            <Tooltip title="Should Include Blanks ensures that every name is displayed an equal number of times and will fill empty spaces with a hyphen">
+              <FormControlLabel control={
+                <Switch
+                  checked={shouldIncludeBlanks}
+                  onChange={(event) => setShouldIncludeBlanks(event.target.checked)}
+                  inputProps={{ 'aria-label': 'controlled' }}
+                />
+              } label="Should Include Blanks" />
+            </Tooltip>
+          </Stack>
+        </Box>
+        <Container maxWidth="xl" style={{ padding: '1em'}}>
+		  <Grid container spacing={1} columns={11}>
+            {
+              loading === true
+              ? (
+                <Box sx={{ width: '100%' }}>
+                  <LinearProgress />
+                </Box>
+              )
+              : Array.from(Array(121)).map((_, index) => getGridItem(index, names))
+            }
+		  </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          May the odds be ever in your favor
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
